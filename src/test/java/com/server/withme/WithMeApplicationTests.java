7138bf15b98ba5c4804f391a4ddda0921e179fb6
BaekJongSeong <jsbaek01@ajou.ac.kr>
2022-03-18 01:45:25 +0900

    init withme-server project

---
+++ src/test/java/com/server/withme/WithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.withme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
