b908d32f3134dd4282b6dace1647ad04bded04ff
BaekJongSeong <jsbaek01@ajou.ac.kr>
2022-04-02 19:35:25 +0900

    feat: schedualr controller update

--- src/main/java/com/server/withme/controller/SchedularController.java
+++ src/main/java/com/server/withme/controller/SchedularController.java
@@ -2,15 +2,22 @@ package com.server.withme.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.server.withme.entity.Account;
 import com.server.withme.serivce.IAccountService;
 import com.server.withme.serivce.ISafeZoneService;
 
+import kr.ac.ajou.vhex.server.model.AccountListDto;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -37,4 +44,30 @@ public class SchedularController {
 		safeZoneService.updateSafeZone(checkedAccountList);
 	}
 
+	@GetMapping("/accounts")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<AccountListDto> accounts(
+            @RequestParam(value = "page", defaultValue = "0") int offset,
+            @RequestParam(value = "size", defaultValue = "10") int limit
+    ) {
+        Pageable pageable = PageRequest.of(offset, limit);
+        AccountListDto dto = new AccountListDto();
+        dto.setElements(accountService.getAccountList(pageable));
+        dto.setCount(accountService.getAccountCount());
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/my-account")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<Account> myAccount() {
+        return ResponseEntity.ok(accountService.getMyAccount());
+    }
+
+    @GetMapping("/accounts/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<Account> accountWithUsername(
+            @PathVariable String username
+    ) {
+        return ResponseEntity.ok(accountService.getAccountByUsernameOrThrow(username));
+    }
 }
