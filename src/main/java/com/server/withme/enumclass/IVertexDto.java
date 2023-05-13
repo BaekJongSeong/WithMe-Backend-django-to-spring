eb2ee576cce01b1919711927b7eae8518b12cd1a
백종성 <jsbaek01@naver.com>
2023-05-13 14:30:20 +0900

    Update IVertexDto.java

--- src/main/java/com/server/withme/enumclass/IVertexDto.java
+++ src/main/java/com/server/withme/enumclass/IVertexDto.java
@@ -1,7 +1,10 @@
 package com.server.withme.enumclass;
 
-public interface IVertexDto {
+public interface DtoD {
 
 	public double getLatitude();
 	public double getLongitude();
+	public double getLat();
+	public double setLatitude();
+	public double getude();
 }
